9df2350ac338293f0fd9728a29baa5e1087848a3
sasama <mametaku3@yahoo.co.jp>
2025-08-10 20:08:41 +0900

    通知処理を追加。またタイマー処理と通知処理を分けて実装

--- sasa-tools/src/components/PomodoroTimer/PomodoroTimer.tsx
+++ sasa-tools/src/components/PomodoroTimer/PomodoroTimer.tsx
@@ -10,32 +10,48 @@ function PomodoroTimer() {
   const [isRunning, setIsRunning] = useState(false); // タイマー動作中か
   const [mode, setMode] = useState<"work" | "break">("work"); // 作業/休憩モード
 
+  // 初回マウント時に通知許可をリクエスト
+  useEffect(() => {
+    // NotificationAPIに対応しているか確認（windowオブジェクト内にNotificationプロパティがあれば対応している）
+    if ("Notification" in window) {
+      Notification.requestPermission();
+    }
+  }, []);
+
   // --- タイマーの処理 ---
   // 第二引数の依存配列に指定している、isRunnningやmodeの値が変わるタイミングで第一引数のコールバック関数が実行される
   useEffect(() => {
     if (!isRunning) return;
 
     const timer = setInterval(() => {
-      setTimeLeft((prev) => {
-        if (prev > 0) {
-          return prev - 1;
-        } else {
-          // 時間終了 → モード切替
-          if (mode === "work") {
-            setMode("break");
-            return BREAK_TIME;
-          } else {
-            setMode("work");
-            return WORK_TIME;
-          }
-        }
-      });
+      setTimeLeft((prev) => prev > 0 ? prev -1 : 0);
     }, 1000);
 
     // クリーンアップ（停止処理）
     // 不要な処理が動き続けるのを防ぐため、前のuseEffectで動かしていたsetIntervalを取り消している。
     return () => clearInterval(timer);
-  }, [isRunning, mode]);
+  }, [isRunning]);
+
+  // タイマー終了時処理
+  useEffect(() => {
+    if (timeLeft === 0) {
+      if (mode === "work") {
+        setMode("break");
+        sendNotification("25分間の作業終了です。5分間休憩しましょう。");
+        setTimeLeft(BREAK_TIME);
+      } else {
+        setMode("work");
+        sendNotification("5分間の休憩終了です。25分間集中して取り組みましょう。");
+        setTimeLeft(WORK_TIME);
+      }
+    }
+  }, [timeLeft, mode]);
+
+  const sendNotification = (message: string) => {
+    if ("Notification" in window && Notification.permission === "granted") {
+      new Notification(message);
+    }
+  };
 
   // --- 時間表示を mm:ss に変換 ---
   const formatTime = (seconds: number) => {

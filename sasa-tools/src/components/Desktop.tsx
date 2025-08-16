5de7c07ef5cc13f172f778cdbfe9f1a3c6f32feb
sasama <mametaku3@yahoo.co.jp>
2025-08-16 17:05:21 +0900

    keyを指定

--- sasa-tools/src/components/Desktop.tsx
+++ sasa-tools/src/components/Desktop.tsx
@@ -15,7 +15,7 @@ export default function Desktop({ icons }: DesktopProps) {
   return (
     <div style={{ padding: 16, display: 'flex', flexWrap: 'wrap' }}>
       {icons.map((icon) => (
-        <DesktopIconItem id={icon.id} title={icon.title} icon={icon.icon} content={icon.content}></DesktopIconItem>
+        <DesktopIconItem key={icon.id} id={icon.id} title={icon.title} icon={icon.icon} content={icon.content}></DesktopIconItem>
       ))}
     </div>
   );

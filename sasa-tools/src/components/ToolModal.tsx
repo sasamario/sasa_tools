bd67dde4f50c294fee3d077b6dd7bd7d0ad168e3
sasama <mametaku3@yahoo.co.jp>
2025-08-16 17:42:15 +0900

    モーダルの高さ、横幅指定を削除

--- sasa-tools/src/components/ToolModal.tsx
+++ sasa-tools/src/components/ToolModal.tsx
@@ -10,22 +10,19 @@ type ToolModalProps = {
 };
 
 export default function ToolModal({ isOpen, id, content, title, onClose }: ToolModalProps) {
-  const modalWidth = 600;
-  const modalHeight = 500;
-
   if (!isOpen) return null;
 
   return (
     <>
       <Frame display="flex" flexDirection="column" gap="16px" p="20px" style={{ padding: 0 }}>
         {/* @ts-ignore */}
-        <Modal title={title} titleBarOptions={[<Modal.Minimize key="minimize" />, <TitleBar.Close onClick={onClose} />]} dragOptions={{
+        <Modal key={id} title={title} titleBarOptions={[<Modal.Minimize key="minimize" />, <TitleBar.Close onClick={onClose} />]} dragOptions={{
           defaultPosition: {
-            x: (window.innerWidth - modalWidth) / 2,
-            y: (window.innerHeight - modalHeight) / 2 - modalHeight / 3
+            x: window.innerWidth / 3,
+            y: window.innerHeight / 4
           }
         }}>
-          <Modal.Content width={`${modalWidth}px`} height={`${modalHeight}px`} boxShadow="$in" bgColor="white" p="16px">
+          <Modal.Content boxShadow="$in" bgColor="white" p="16px">
             <Frame as="div" display="flex" flexDirection="column" gap="8px">
               {content}
             </Frame>

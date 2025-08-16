fa1c5c04a42fa5731b3856820be8b689b7708509
sasama <mametaku3@yahoo.co.jp>
2025-08-16 17:06:23 +0900

    position: fixedによって、モーダルが1つしか表示されなくなってしまったため修正。アイコン間の余白はFlameタグでスタイルを適用することで解消

--- sasa-tools/src/components/ToolModal.tsx
+++ sasa-tools/src/components/ToolModal.tsx
@@ -17,24 +17,21 @@ export default function ToolModal({ isOpen, id, content, title, onClose }: ToolM
 
   return (
     <>
-      {/* モーダル要素はDOMの通常フローから独立させるためposition: fixedを適用した要素で囲っている */}
-      <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
-        <Frame display="flex" flexDirection="column" gap="16px" p="20px">
-          {/* @ts-ignore */}
-          <Modal id={id} title={title} titleBarOptions={<TitleBar.Close onClick={onClose} />} dragOptions={{
-            defaultPosition: {
-              x: (window.innerWidth - modalWidth) / 2,
-              y: (window.innerHeight - modalHeight) / 2 - modalHeight / 3
-            }
-          }}>
-            <Modal.Content width={`${modalWidth}px`} height={`${modalHeight}px`} boxShadow="$in" bgColor="white" p="16px">
-              <Frame as="div" display="flex" flexDirection="column" gap="8px">
-                {content}
-              </Frame>
-            </Modal.Content>
-          </Modal>
-        </Frame>
-      </div>
+      <Frame display="flex" flexDirection="column" gap="16px" p="20px" style={{ padding: 0 }}>
+        {/* @ts-ignore */}
+        <Modal title={title} titleBarOptions={[<Modal.Minimize key="minimize" />, <TitleBar.Close onClick={onClose} />]} dragOptions={{
+          defaultPosition: {
+            x: (window.innerWidth - modalWidth) / 2,
+            y: (window.innerHeight - modalHeight) / 2 - modalHeight / 3
+          }
+        }}>
+          <Modal.Content width={`${modalWidth}px`} height={`${modalHeight}px`} boxShadow="$in" bgColor="white" p="16px">
+            <Frame as="div" display="flex" flexDirection="column" gap="8px">
+              {content}
+            </Frame>
+          </Modal.Content>
+        </Modal>
+      </Frame>
     </>
   );
 }

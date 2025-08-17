4983c8377b5d86401c0396d5ba68952306da3e0f
sasama <mametaku3@yahoo.co.jp>
2025-08-17 13:22:58 +0900

    GithubPagesデプロイ用設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // https://ja.vite.dev/guide/static-deploy.html#github-pages
+  base: '/sasa_tools/',
 })

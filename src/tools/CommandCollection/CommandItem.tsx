1cdffef0eff14f4934a7dd94d5e51fa5e499d32f
sasama <mametaku3@yahoo.co.jp>
2025-08-24 14:52:47 +0900

    コピーしたことがわかるよう、コピー時にボタンのアイコンをチェックマークにする

--- src/tools/CommandCollection/CommandItem.tsx
+++ src/tools/CommandCollection/CommandItem.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { Frame, Input, Button, Tooltip } from "@react95/core";
-import { Copy } from "@react95/icons";
+import { Copy, Tick } from "@react95/icons";
 
 type CommandItemProps = { 
   command: string,
@@ -7,14 +8,25 @@ type CommandItemProps = {
 };
 
 export default function CommandItem({ command, description }: CommandItemProps) {
-  const copyCommand = () => navigator.clipboard.writeText(command);
+  const [copied, setCopied] = useState(false);
+
+  const copyCommand = async () => {
+    try {
+      await navigator.clipboard.writeText(command);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    } catch (error) {
+      console.error('コピーに失敗しました', error);
+    }
+  }
+
   return (
     <Frame display={'flex'} gap="$4" mb="$4">
       <Tooltip text={description} delay={100} style={{ width: '100%'}}>
         <Input w="100%" h="100%" fontFamily="monospace" value={command} readOnly/>
       </Tooltip>
       <Button onClick={copyCommand}>
-        <Copy />
+        {copied ? <Tick variant="16x16_4" /> : <Copy variant="16x16_4" />}
       </Button>
     </Frame>
   );

56c3ec8200f5b371ab8a0a8153b8383de47836f1
sasama <mametaku3@yahoo.co.jp>
2025-09-05 18:20:06 +0900

    光明泉の露天風呂情報を表示する機能を追加

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,8 @@ import '@react95/core/themes/win95.css';
 // icons のスタイルだけ読み込む（coreのスタイルは組み込み済み）
 import '@react95/icons/icons.css';
 
+import './reset.css'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
